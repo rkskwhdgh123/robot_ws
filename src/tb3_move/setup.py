b53fc8c0e78d824bd8d63b4ee52273a6b214c560
Your Name <rkskwhdgh123@gmail.com>
2023-01-18 02:13:51 -0800

    2023-01-18

---
+++ src/tb3_move/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+package_name = 'tb3_move'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubumtu',
+    maintainer_email='gooda1900@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'tb3m = tb3_move.tb3_basic_move:main',
+            'tb3s = tb3_move.tb3_sub:main',
+            'tb3mix = tb3_move.tb3_mix:main',
+        ],
+    },
+)
+
